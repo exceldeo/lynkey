840760259f161f5e531cfde70d7d3ea77bfb24d4
exceldeo <excel.deo99@gmail.com>
2024-06-28 02:13:43 +0700

    refactor: Improve SearchBar component with clear search functionality

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -13,13 +13,28 @@ export default function SearchBar({ onSearch }: SearchBarProps) {
     onSearch(event.target.value);
   }
 
+  function handleClearSearch() {
+    setQuery("");
+    onSearch("");
+  }
+
   return (
+    <div className="relative">
       <Input
         type="text"
         value={query}
         onChange={handleInputChange}
         placeholder="Search..."
-        className="p-2 rounded-3xl shadow-sm border-gray-500 dark:border-gray-700 dark:bg-slate-700 dark:text-white border-2"
+        className="p-3 rounded-3xl shadow-sm border-gray-500 dark:border-gray-700 dark:bg-slate-700 dark:text-white border-2 pr-7"
       />
+      {query && (
+        <div
+          className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer text-red-500"
+          onClick={handleClearSearch}
+        >
+          x
+        </div>
+      )}
+    </div>
   );
 }
